d289eb678528046deff3067c72de5029093d6f92
Shayan Dsouza <dsouza.shayan@gmail.com>
2024-07-03 13:02:18 -0400

    added a player turn function to swap between players

--- main.py
+++ main.py
@@ -8,7 +8,53 @@ PIXEL_WIDTH = WINDOW_WIDTH // 3
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_WIDTH))
 clock = pygame.time.Clock()
 
-font = pygame.font.Font('comicsansmsbold.ttf', 32)
+font = pygame.font.SysFont('comicsans', 32)
 winner_text = font.render('', True, 'green')
 textRect = winner_text.get_rect()
 textRect.center = (WINDOW_WIDTH // 2 - PIXEL_WIDTH // 2, WINDOW_WIDTH // 2)
+
+running = True
+
+board = [
+    [None, None, None],
+    [None, None, None],
+    [None, None, None]
+]
+
+
+def load_icon(path, resolution):
+    icon = pygame.image.load(path)
+    return pygame.transform.scale(icon, resolution)
+
+
+ICON_X = load_icon('x-icon.png', [PIXEL_WIDTH, PIXEL_WIDTH])
+ICON_O = load_icon('o-icon.png', [PIXEL_WIDTH, PIXEL_WIDTH])
+GRID = load_icon('grid.jpg', [WINDOW_WIDTH, WINDOW_WIDTH])
+
+PLAYER_1 = 0
+PLAYER_2 = 1
+player = PLAYER_1
+
+
+def play_turn(current_player):
+    curr_coordinate = pygame.math.Vector2(pygame.mouse.get_pos())
+    normalized_coordinate = curr_coordinate // PIXEL_WIDTH
+    if pygame.mouse.get_pressed()[0]:
+        col, row = map(int, normalized_coordinate)
+        board[row][col] = current_player
+        global player
+        player = 1 - player
+
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # RENDER YOUR GAME HERE
+    pygame.display.flip()
+    screen.fill("white")
+    screen.blit(GRID, (0, 0))
+    pygame.event.wait()
+
+pygame.quit()
